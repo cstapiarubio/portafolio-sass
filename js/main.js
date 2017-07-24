df8799aeebc10e2b7d7fbe20142f7e77567a3bb2
cstapiarubio <cstapiarubio@gmail.com>
2017-07-24 12:05:17 -0400

    lista scroll nav

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
 /* fx nav color scroll*/
+$(document).ready(function() {
+$(window).scroll(function() {
+        $('#nav').toggleClass('nav-trans', $(this).scrollTop() > $('#nav').height());
+      });
+});
 
-var myNav = document.getElementById('nav-dos');
-window.onscroll = function () { 
-    "use strict";
-    if (document.body.scrollTop >= 200) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } 
-    else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
-    }
-};
 
 
 /* fx carousel*/
